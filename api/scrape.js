a371b1033b7ab7db7e105ed5e52fa8d84216d921
FFerozz <fedorawearinglinuxuser@gmail.com>
2025-07-11 19:17:00 -0500

    Update scrape.js

--- api/scrape.js
+++ api/scrape.js
@@ -2,12 +2,19 @@ import axios from 'axios';
 import * as cheerio from 'cheerio';
 
 export default async function handler(req, res) {
+
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
+
     return res.status(200).end();
+  }
+
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
 
   const { url } = req.query;
 
